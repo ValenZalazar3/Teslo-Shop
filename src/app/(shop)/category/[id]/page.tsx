e842423c7bdc91f7117626ac81aa337ee9e1db98
Valen <valenzalazar3@gmail.com>
2024-05-04 21:06:04 -0300

    Some fictures done (slideshow, size and quantity selector)

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,11 +1,14 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 
-
+const seedProductos = initialData.products;
 
 interface Props {
     params: {
-        id: string;
+        id: Category;
     }
 }
 
@@ -15,14 +18,27 @@ interface Props {
 export default function ({ params }: Props) {
 
     const { id } = params
-
-    if (id === 'kids') {
-        notFound()
+    const products = seedProductos.filter(product => product.gender === id)
+    const labels: Record<Category, string> = {
+        'men': 'para hombres',
+        'women': 'para mujeres',
+        'kid': 'para niños',
+        'unisex': 'para todos'
     }
+    // if (id === 'kids') {
+    //     notFound()
+    // }
     return (
-        <div>
-            <h1>Category Page</h1>
-        </div>
+        <>
+            <Title
+                title={`Artículos ${labels[id]}`}
+                subtitle="Todos los productos"
+                className="mb-2"
+
+            />
+
+            <ProductGrid products={products} />
+        </>
     )
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,8 +4,17 @@ export * from './ui/top-menu/TopMenu'
 export * from './ui/not-found/PageNotfound'
 export * from './ui/title/Title'
 export * from './ui/sidebar/SideBar'
+
+
 export * from './products/product-grid/ProductGrid'
 export * from './products/product-grid/ProductGridItem'
+
+export * from './product/size-selector/SizeSelector'
+
+export * from './product/quantity-selector/QuantitySelector'
+
+export * from './product/slide-show/ProductSlideShow'
+export * from './product/slide-show/ProductMobileSlideShow'
 
 
 
