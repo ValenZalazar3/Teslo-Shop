d88c2ba39f40f262c54342542f639f34bf906c58
Valen <valenzalazar3@gmail.com>
2024-05-06 18:03:10 -0300

    UI done

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,12 +1,15 @@
 
 
 
-export default function ShopLayout({children}:{
-    children:React.ReactNode
+export default function ShopLayout({ children }: {
+    children: React.ReactNode
 }) {
-    return(
-        <main className="min-h-screen bg-gray-500">
-           {children}
+    return (
+        <main className="flex justify-center">
+            <div className="w-full sm:w-[350px] px10">
+
+                {children}
+            </div>
         </main>
     )
 }
